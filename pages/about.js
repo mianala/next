9337bf2c33d7f835f2c425ab9033a31e02a4bccb
Loharano <mianalaloharano@gmail.com>
2022-02-05 17:41:15 +0300

    pages

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos rem
+        omnis beatae quaerat laboriosam sequi ipsum consequatur mollitia commodi
+        odio excepturi tenetur quasi ut porro, nesciunt vitae cum sapiente
+        velit!
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ pages/sub/test.js
@@ -0,0 +1,14 @@
+const Test = () => {
+  return (
+    <div>
+      <h1>Sub/Test</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+        excepturi dolores possimus at illo enim id hic magni soluta adipisci? At
+        velit autem dolores, incidunt eius minima adipisci magni optio.
+      </p>
+    </div>
+  )
+}
+
+export default Test
